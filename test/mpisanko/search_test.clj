3211c892a2af401e230d91f11878f1e95df12c7d
Michal Pisanko <mpisanko@gmail.com>
2020-04-19 17:35:23 +1000

    test other functions

--- test/mpisanko/search_test.clj
+++ test/mpisanko/search_test.clj
@@ -1,6 +1,9 @@
 (ns mpisanko.search-test
   (:require [clojure.test :refer :all]
-            [mpisanko.search :as s]))
+            [mpisanko.search :as s]
+            [mpisanko.index :as index]
+            [clojure.java.io :as io])
+  (:import (java.io File)))
 
 (def entities {1 {:_id 1 :name "abc" :details "foo" :tags ["666" "667"]}
                2 {:_id 2 :name "def" :details "bar" :tags []}
@@ -35,3 +38,30 @@
 
   (testing "returns none if all entities have the field not empty"
     (is (empty? (#'s/find-empty entities "_id")))))
+
+(def index-invoked (atom 0))
+(def original-reader io/reader)
+
+(deftest read-index-test
+  (with-redefs [index/create (fn [] (swap! index-invoked inc))
+                io/reader (fn [_] (original-reader (char-array "{:a 1}")))]
+    (testing "when the index does not exist it invokes indexer to create it"
+     (is (= {:a 1} (#'s/read-index "foobar")))
+     (is (= 1 @index-invoked))
+     (reset! index-invoked 0))
+
+    (testing "when the index does exist it just reads it without invoking indexer"
+      (spit "foobar-index.edn" "{:a 1}")
+      (is (= {:a 1} (#'s/read-index "foobar")))
+      (is (zero? @index-invoked))
+      (.delete (File. "foobar-index.edn")))))
+
+(deftest query-test
+  (with-redefs [s/read-index (fn [_]  {:index index :entities entities})]
+    (testing "querying for a field"
+      (let [results (s/query {:organisation true} ["foo"])]
+        (is (= 2 (count results)))))
+
+    (testing "querying for empty field"
+      (let [results (s/query {:organisation true :empty true} ["tags"])]
+        (is (= 1 (count results)))))))
